63a191141f46eb6436066571e73caba59a3b6ca2
kevin.stanton <kevin.stanton@mottmac.com>
2022-03-02 14:55:00 -0800

    WIP

--- src/parse_pdf.py
+++ src/parse_pdf.py
@@ -13,3 +13,15 @@ file = '122475_3919_DataTables.pdf'
 
 tables = tabula.read_pdf(file, pages="all")
 
+page_1_table = tables[0]
+
+
+def format_table(table_object):
+    
+    # 1. Create template dataframe (where the results are stored)
+    ##   - columns headers = ID_Short, STATION, SAMPLE_ID, TOP_ft, BOT_ft, SampleType, Analyte, Units, Result, Qualifier, Cite
+    result = pd.DataFrame(columns=['ID_Short', 'STATION', 'SAMPLE_ID', 'TOP_ft', 'BOT_ft', 'SampleType', 'Analyte', 'Units', 'Result', 'Qualifier', 'Cite'])
+    
+    # 2. Populate template with data taken from 'table_object'
+    
+
