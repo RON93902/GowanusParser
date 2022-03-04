5fb9ad9273be37ecf3c71dbf9a19750a18b6d2f1
kevin.stanton <kevin.stanton@mottmac.com>
2022-03-04 15:53:41 -0800

    resolve conflicts

--- src/parse_pdf.py
+++ src/parse_pdf.py
@@ -9,36 +9,16 @@ import tabula
 import os
 import pandas as pd
 import numpy as np
-<<<<<<< .merge_file_a21212
-# import re
-# import sys
-# from PyPDF2 import PdfFileWriter, PdfFileReader
-=======
+import re
 import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
-import re
->>>>>>> .merge_file_a20040
+
 
 file = '122475_3919_DataTables.pdf'
 template = 'cleaned.tabula-template.json'
 
-<<<<<<< .merge_file_a21212
-# # Clean pdf file
-# pdf_in = PdfFileReader(file,'rb')
-# pdf_out = PdfFileWriter()
-# for p in [pdf_in.getPage(i) for i in range(0, pdf_in.getNumPages())]:
-#     text = p.extractText()
-#     if not re.search(r'Page Intentionally Left Blank', text, re.I):
-#         pdf_out.addPage(p)
-# with open('cleaned.pdf', 'wb') as f:
-#     pdf_out.write(f)
-
 # Read in tables from pdf file
 tables = tabula.read_pdf(file, pages="all")
-
-# table_I9A, index 0 to 44 in tables
-# table_I10A, index 45 to 67 in tables
-# table_I12A, index 68 to 88 in tables
 
 # create a list to store all the complete tables in PDF file
 target = []
@@ -50,8 +30,6 @@ for i in range(45):
         j = j + 1
 
     target[j] = target[j].append(tables[i-1].append(tables[i][6:], ignore_index = True) )
-        
-=======
 
 # Read in tables from cleaned pdf
 tables = tabula.read_pdf_with_template(file, template)
@@ -81,9 +59,7 @@ for i in range(0,len(tables)-1):
         print('table ', i)
 
 df = tables[0]
-a = df.where(df=='GC-SED-83').dropna(how='all')
->>>>>>> .merge_file_a20040
-        
+a = df.where(df=='GC-SED-83').dropna(how='all')      
       
         
 # endOfTable = 'no'
