71d01e6e81212b6f29a1fd9fdb047a12697322bb
RON93902 <wenyong.rong@mottmac.com>
2022-03-04 15:41:09 -0800

    WIP

--- src/parse_pdf.py
+++ src/parse_pdf.py
@@ -9,44 +9,32 @@ import tabula
 import os
 import pandas as pd
 import numpy as np
-import sys
-from PyPDF2 import PdfFileWriter, PdfFileReader
-import re
+# import re
+# import sys
+# from PyPDF2 import PdfFileWriter, PdfFileReader
 
 file = '122475_3919_DataTables.pdf'
 
+# # Clean pdf file
+# pdf_in = PdfFileReader(file,'rb')
+# pdf_out = PdfFileWriter()
+# for p in [pdf_in.getPage(i) for i in range(0, pdf_in.getNumPages())]:
+#     text = p.extractText()
+#     if not re.search(r'Page Intentionally Left Blank', text, re.I):
+#         pdf_out.addPage(p)
+# with open('cleaned.pdf', 'wb') as f:
+#     pdf_out.write(f)
 
-# Clean pdf file
-pdf_in = PdfFileReader(file,'rb')
-pdf_out = PdfFileWriter()
-for p in [pdf_in.getPage(i) for i in range(0, pdf_in.getNumPages())]:
-    text = p.extractText()
-    if not re.search(r'Page Intentionally Left Blank', text, re.I):
-        pdf_out.addPage(p)
-with open('cleaned.pdf', 'wb') as f:
-    pdf_out.write(f)
-
-# Read in tables from cleaned pdf
-tables = tabula.read_pdf('cleaned.pdf', pages="all")
+# Read in tables from pdf file
+tables = tabula.read_pdf(file, pages="all")
 
 # table_I9A, index 0 to 44 in tables
 # table_I10A, index 45 to 67 in tables
 # table_I12A, index 68 to 88 in tables
 
-#test changes for learning git
-
-page_1_table = tables[0]
-page_2_table = tables[1]
-
-# def format_table(table_object):
-    
-#     # 1. Create template dataframe (where the results are stored)
-#     ##   - columns headers = ID_Short, STATION, SAMPLE_ID, TOP_ft, BOT_ft, SampleType, Analyte, Units, Result, Qualifier, Cite
-#     result = pd.DataFrame(columns=['ID_Short', 'STATION', 'SAMPLE_ID', 'TOP_ft', 'BOT_ft', 'SampleType', 'Analyte', 'Units', 'Result', 'Qualifier', 'Cite'])
-    
-#     # 2. Populate template with data taken from 'table_object'
-
+# create a list to store all the complete tables in PDF file
 target = []
+
 target.append(tables[0][:6])
 j = 0
 for i in range(45):
@@ -54,8 +42,6 @@ for i in range(45):
         j = j + 1
 
     target[j] = target[j].append(tables[i-1].append(tables[i][6:], ignore_index = True) )
-    table_I9A[2*i-1]= temp_dataFrame
-        
         
         
       
