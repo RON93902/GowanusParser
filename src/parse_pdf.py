d0479913e063a561297567e36411d5b272f47e22
kevin.stanton <kevin.stanton@mottmac.com>
2022-03-04 18:09:31 -0800

    wip

--- src/parse_pdf.py
+++ src/parse_pdf.py
@@ -13,14 +13,29 @@ import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 import re
 
-file = '122475_3919_DataTables.pdf'
+file = '122475_3919_Datatables.pdf'
 template = 'cleaned.tabula-template.json'
 
 
-# Read in tables from cleaned pdf
-tables = tabula.read_pdf_with_template(file, template)
-for i in range(0,len(tables)):
-    tables[i] = tables[i].T.reset_index().T
+# Read in tables_raw from cleaned pdf
+tables_raw = tabula.read_pdf_with_template(file, template)
+for i in range(0,len(tables_raw)):
+    tables_raw[i] = tables_raw[i].T.reset_index().T
+
+
+
+tables_clean = []
+tables_clean.append(tables_raw[0][:6])
+j = 0
+for i in range(0,len(tables_raw)):
+    
+    if not tables_raw[i].iloc[0,0] == 'Station Location:':
+        tables_clean[j] = tables_clean[j].append(tables_raw[i], ignore_index = True)
+    
+    if ('Total' in tables_raw[i].iloc[len(tables_raw[i].index)-1,0]):
+        j = j + 1
+        
+    
 
 # # def format_table(table_object):
     
@@ -29,33 +44,14 @@ for i in range(0,len(tables)):
 # #     result = pd.DataFrame(columns=['ID_Short', 'STATION', 'SAMPLE_ID', 'TOP_ft', 'BOT_ft', 'SampleType', 'Analyte', 'Units', 'Result', 'Qualifier', 'Cite'])
     
 # #     # 2. Populate template with data taken from 'table_object'
-
-# target = []
-# target.append(tables[0][:6])
-# j = 0
-# for i in range(45):
-#     if ('Total' in tables[i].iloc[len(tables[i].index)-1,0]):
-#         j = j + 1
-
-#     target[j] = target[j].append(tables[i-1].append(tables[i][6:], ignore_index = True) )
-#     table_I9A[2*i-1]= temp_dataFrame
-        
-for i in range(0,len(tables)-1):
-    if (tables[i] == 'GEC-SD-98').any().idxmax() > 0:
-        print('table ', i)
-
-df = tables[0]
-a = df.where(df=='GC-SED-83').dropna(how='all')
-        
-      
         
 # endOfTable = 'no'
-# for i in range(0,len(tables)-1):
+# for i in range(0,len(tables_raw)-1):
 #     if endOfTable == 'yes':
-#         temp_dataFrame = tables[i][0:0]
-#     if 'Total' in tables[i].iloc[len(tables[i].index)-1,0]:
+#         temp_dataFrame = tables_raw[i][0:0]
+#     if 'Total' in tables_raw[i].iloc[len(tables_raw[i].index)-1,0]:
 #         print('yes')
 #         endOfTable = 'yes'
-#     # temp_DataFrame.append(tables[i].rows[6:])
+#     # temp_DataFrame.append(tables_raw[i].rows[6:])
 
 # tabula.convert_into(pdf_path, "test.csv", output_format="csv", stream=True)
