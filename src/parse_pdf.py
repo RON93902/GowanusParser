70d5d4d03e4dbe80ae3047c47874e8576e4a9514
RON93902 <wenyong.rong@mottmac.com>
2022-03-07 10:17:39 -0800

    temp

--- src/parse_pdf.py
+++ src/parse_pdf.py
@@ -13,62 +13,29 @@ import re
 import sys
 from PyPDF2 import PdfFileWriter, PdfFileReader
 
-
+# Extract PDF table in the format of the template
 file = '122475_3919_DataTables.pdf'
 template = 'cleaned.tabula-template.json'
+tables_raw = tabula.read_pdf_with_template(file, template)
 
-# Read in tables from pdf file
-tables = tabula.read_pdf(file, pages="all")
+# Cleanup the read in dataframe
+for i in range(0,len(tables_raw)):
+    tables_raw[i] = tables_raw[i].T.reset_index().T
+    tables_raw[i].reset_index(drop=True, inplace=True)
 
-# create a list to store all the complete tables in PDF file
-target = []
+for i in range(0,len(tables_raw)):
+    if ('Sample' in tables_raw[i].iloc[len(tables_raw[i].index)-1,0]):
+        tables_raw[i].insert(0,'','')
 
-target.append(tables[0][:6])
-j = 10
-for i in range(45):
-    if ('Total' in tables[i].iloc[len(tables[i].index)-1,0]):
-        j = j + 1
+# create a list that will store the complete tables as shown in PDF
 
-    target[j] = target[j].append(tables[i-1].append(tables[i][6:], ignore_index = True) )
-
-# Read in tables from cleaned pdf
-tables = tabula.read_pdf_with_template(file, template)
-for i in range(0,len(tables)):
-    tables[i] = tables[i].T.reset_index().T
-
-# # def format_table(table_object):
-    
-# #     # 1. Create template dataframe (where the results are stored)
-# #     ##   - columns headers = ID_Short, STATION, SAMPLE_ID, TOP_ft, BOT_ft, SampleType, Analyte, Units, Result, Qualifier, Cite
-# #     result = pd.DataFrame(columns=['ID_Short', 'STATION', 'SAMPLE_ID', 'TOP_ft', 'BOT_ft', 'SampleType', 'Analyte', 'Units', 'Result', 'Qualifier', 'Cite'])
-    
-# #     # 2. Populate template with data taken from 'table_object'
-
-# target = []
-# target.append(tables[0][:6])
 # j = 0
-# for i in range(45):
-#     if ('Total' in tables[i].iloc[len(tables[i].index)-1,0]):
+# # tables_clean.append(tables_raw[0])
+# for i in range(0,len(tables_raw)):
+#     if ('Total' not in tables_raw[i].iloc[len(tables_raw[i].index)-1,0]):
+#         tables_clean = tables_clean.concat(tables_raw[i],ignore_index=True,sort=False)
+#     if ('Total' in tables_raw[i].iloc[len(tables_raw[i].index)-1,0]):
+#         tables_clean[j] = tables_clean[j].append(tables_raw[i],ignore_index=True,sort=False)
 #         j = j + 1
 
-#     target[j] = target[j].append(tables[i-1].append(tables[i][6:], ignore_index = True) )
-#     table_I9A[2*i-1]= temp_dataFrame
-        
-for i in range(0,len(tables)-1):
-    if (tables[i] == 'GEC-SD-98').any().idxmax() > 0:
-        print('table ', i)
-
-df = tables[0]
-a = df.where(df=='GC-SED-83').dropna(how='all')      
-      
-        
-# endOfTable = 'no'
-# for i in range(0,len(tables)-1):
-#     if endOfTable == 'yes':
-#         temp_dataFrame = tables[i][0:0]
-#     if 'Total' in tables[i].iloc[len(tables[i].index)-1,0]:
-#         print('yes')
-#         endOfTable = 'yes'
-#     # temp_DataFrame.append(tables[i].rows[6:])
-
-# tabula.convert_into(pdf_path, "test.csv", output_format="csv", stream=True)
+# # tabula.convert_into(pdf_path, "test.csv", output_format="csv", stream=True)
